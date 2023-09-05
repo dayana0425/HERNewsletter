d4f2579de733dfde676bf32199178a0bf7183604
Daiana Bilbao <hellodayana0425@gmail.com>
2023-09-05 10:15:54 -0600

    Untrack the file/folder

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rowkebbvlubpvsigkqtb.supabase.co';
-const supabaseKey = 'your-supabase-key-here';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!;
+
+if (!supabaseUrl) {
+  throw new Error('You need to provide a SUPABASE_URL env variable');
+}
+
+if (!supabaseKey) {
+  throw new Error('You need to provide a SUPABASE_KEY env variable');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
