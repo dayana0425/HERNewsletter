38ca3a91209eb1f361729cabeab5149c21f4ad04
Daiana Bilbao <hellodayana0425@gmail.com>
2023-09-05 10:31:16 -0600

    add

--- src/config/client.ts
+++ src/config/client.ts
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('Supabase Key:', import.meta.env.VITE_SUPABASE_KEY);
+
 if (!supabaseKey) {
   throw new Error('You need to provide a SUPABASE_KEY env variable');
 }
