0a6faba614b451132d83db4ed833ee33b5c4c588
Daiana Bilbao <hellodayana0425@gmail.com>
2023-09-05 21:12:58 -0600

    automatically prompt switch network

--- src/features/loginGuard.tsx
+++ src/features/loginGuard.tsx
@@ -1,42 +1,71 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect } from 'react';
 import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
 import { Box } from '@mui/material';
-
-/* 
-This component is used to switch the network to the right one.
-*/
 
 const LoginGuard: FC<{ children: ReactNode }> = ({ children }) => {
   const { isConnected } = useAccount();
   const { chain } = useNetwork();
-  const { chains, error, isLoading, pendingChainId, switchNetwork } =
-    useSwitchNetwork();
+  const { chains, switchNetwork } = useSwitchNetwork();
 
   const handleSwitchNetwork = (chainId: number) => {
     switchNetwork?.(chainId);
   };
 
+  useEffect(() => {
+    if (isConnected && chain?.id !== 134) {
+      handleSwitchNetwork(chains[0]?.id);
+    }
+  }, [isConnected, chain]);
+
   return (
     <>
       {children}
-      {isConnected && chain?.id !== 134 && (
-        <Box sx={{ margin: '3rem auto' }}>
-          <p>Oops, you're on the wrong network</p>
-          <p> Click on the following button to switch to the right network </p>
-          <button
-            className="switch-network-button"
-            disabled={!switchNetwork || chain?.id === chains[0]?.id}
-            key={chains[0]?.id}
-            onClick={() => handleSwitchNetwork(chains[0]?.id)}
-          >
-            Switch to {chains[0].name}
-            {isLoading && pendingChainId === chains[0]?.id && ' (switching)'}
-          </button>
-          <div>{error && error.message}</div>
-        </Box>
-      )}
     </>
   );
 };
 
 export default LoginGuard;
+
+
+// import { FC, ReactNode } from 'react';
+// import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
+// import { Box } from '@mui/material';
+
+// /* 
+// This component is used to switch the network to the right one.
+// */
+
+// const LoginGuard: FC<{ children: ReactNode }> = ({ children }) => {
+//   const { isConnected } = useAccount();
+//   const { chain } = useNetwork();
+//   const { chains, error, isLoading, pendingChainId, switchNetwork } =
+//     useSwitchNetwork();
+
+//   const handleSwitchNetwork = (chainId: number) => {
+//     switchNetwork?.(chainId);
+//   };
+
+//   return (
+//     <>
+//       {children}
+//       {isConnected && chain?.id !== 134 && (
+//         <Box sx={{ margin: '3rem auto' }}>
+//           <p>Oops, you're on the wrong network</p>
+//           <p> Click on the following button to switch to the right network </p>
+//           <button
+//             className="switch-network-button"
+//             disabled={!switchNetwork || chain?.id === chains[0]?.id}
+//             key={chains[0]?.id}
+//             onClick={() => handleSwitchNetwork(chains[0]?.id)}
+//           >
+//             Switch to {chains[0].name}
+//             {isLoading && pendingChainId === chains[0]?.id && ' (switching)'}
+//           </button>
+//           <div>{error && error.message}</div>
+//         </Box>
+//       )}
+//     </>
+//   );
+// };
+
+// export default LoginGuard;
